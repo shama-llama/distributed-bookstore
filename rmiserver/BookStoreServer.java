a619794e1653fcfb25181697dbab793dc3ed9c0d
shama-llama <191445184+shama-llama@users.noreply.github.com>
2025-02-13 00:22:28 +0300

    feat: add bookstore server

---
+++ rmiserver/BookStoreServer.java
@@ -0,0 +1,17 @@
+package rmiserver;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+public class BookStoreServer {
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1099);
+            BookStoreImplement bookStore = new BookStoreImplement();
+            Naming.rebind("rmi://host:1099/BookStoreService", bookStore);
+            System.out.println("BookStore Server is running...");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
