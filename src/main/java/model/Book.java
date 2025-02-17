0565da7afb51c34c47ba30841666382e5e230e27
shama-llama <191445184+shama-llama@users.noreply.github.com>
2025-02-17 13:35:09 +0300

    feat: update book model

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -1,30 +1,35 @@
-package rmiserver;
+package model;
+
 import java.io.Serializable;
 
 public class Book implements Serializable {
-    private int id;
+    private String isbn;
     private String title;
     private String author;
+    private int year;
     private double price;
     private int quantity;
 
     public Book() {}
 
-    public Book(int id, String title, String author, double price, int quantity) {
-        this.id = id;
+    public Book(String isbn, String title, String author, int year, double price, int quantity) {
+        this.isbn = isbn;
         this.title = title;
         this.author = author;
+        this.year = year;
         this.price = price;
         this.quantity = quantity;
     }
 
     // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
     public String getAuthor() { return author; }
     public void setAuthor(String author) { this.author = author; }
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
     public double getPrice() { return price; }
     public void setPrice(double price) { this.price = price; }
     public int getQuantity() { return quantity; }
